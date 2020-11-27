6db631e03becf563c17bc874be7ffb54954468f4
Felix Razykov <frazykov@gmail.com>
2020-11-27 17:18:04 +0200

    Use react-bootstrap for worspaces display

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { RecoilRoot } from 'recoil';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './style/index.css';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
