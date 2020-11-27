458a04230533f2eb375d32337d5c7e0795abf996
Felix Razykov <frazykov@gmail.com>
2020-11-27 16:42:09 +0200

    Enable cors for all API requests

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import morgan from 'morgan';
 import helmet from 'helmet';
 import StatusCodes from 'http-status-codes';
 import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
 
 import 'express-async-errors';
 
@@ -14,6 +15,7 @@ initDb();
 const app = express();
 const { BAD_REQUEST } = StatusCodes;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
